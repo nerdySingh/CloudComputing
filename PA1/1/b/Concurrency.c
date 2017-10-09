d9312ec1b80b582d6dd890354b1c4d634c9c6377
Tanny <nerdySingh@users.noreply.github.com>
2017-10-09 01:55:27 -0500

    Concurrency Done.

--- PA1/1/b/Concurrency.c
+++ PA1/1/b/Concurrency.c
@@ -5,9 +5,49 @@
 #define numb_threds 1
 #define Max_iterations 100000000
 int g =0;
+double gflops1=0.0,gflops2=0.0,gflops3;
+
+void *threadFunctionFlop1()
+{
+    double d  = pow(2,-128);
+    clock_t start,end;
+    double cpu_time_used;
+    int i;
+    long iterations = Max_iterations / numb_threds;
+    start = clock();
+    for(i=0;i<Max_iterations;i++)
+    {
+        d = d /d ;
+    }
+    for(i=0;i<Max_iterations;i++)
+    {
+        d = d + d ;
+    }
+    for(i=0;i<Max_iterations;i++)
+    {
+        d = d - d ;
+    }
+    for(i=0;i<Max_iterations;i++)
+    {
+        d = d * d ;
+    }
+    end = clock();
+    cpu_time_used = ((double)(end-start)) / CLOCKS_PER_SEC;
+    printf("\nTime: %f s\n",cpu_time_used);
+    double Flops=(Max_iterations)/(double)(cpu_time_used);
+    long gFlops=(double)Flops/1000000000;// Calculate Giga Flops Formula: F lops * 10raised to (-9).
+    gflops2 = Flops;
+    
+    return NULL;
+}
+
+
+
+
+
 void *threadFunctionFlop(void *arg)
 {
-    printf("This is thread executing the IOPS instruction for Concurrency.%d\n ",(int)arg);
+    //printf("This is thread executing the IOPS instruction for Concurrency.%d\n ",(int)arg);
     long d  = 0x3fd5555555555555L;
     clock_t start,end;
     double cpu_time_used;
@@ -33,11 +73,13 @@ void *threadFunctionFlop(void *arg)
     }
     end = clock();
     cpu_time_used = ((double)(end-start)) / CLOCKS_PER_SEC;
-    printf("Thread ID: %d\n", *myid);
-    printf("Time: %f s\n",cpu_time_used);
+    //printf("Thread ID: %d\n", *myid);
+    //printf("Time: %f s\n",cpu_time_used);
     double Flops=(Max_iterations)/(double)(cpu_time_used);
     long gFlops=(double)Flops/1000000000;// Calculate Giga Flops Formula: F lops * 10raised to (-9).
-    printf("GFLOPS : %f\n",gFlops);
+    gflops1 = Flops +gflops1;
+    threadFunctionFlop1();
+    //printf("GFLOPS : %f\n",gFlops);
     return NULL;
 }
 
@@ -48,7 +90,7 @@ int main()
     int i,no_threads;
     no_threads = numb_threds;
     
-    while(no_threads < 9)
+    while(no_threads < 2)
     {
         pthread_t th[no_threads];
         for(i=0;i<no_threads;i++)
@@ -63,6 +105,12 @@ int main()
 	printf("\n"); 
        no_threads = no_threads * 2;
     }
-    
+   
+    gflops1 = gflops1 /1000000000;
+    gflops2 = gflops2 / 1000000000;
+    printf("GFLOPS : %f\n",gflops1);
+    printf("GFLOPS : %f\n",gflops2);
+ 
+
     return 0;
 }
